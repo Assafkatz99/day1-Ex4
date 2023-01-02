940a98ba1f1444b85cc4004d4d8b336f8218c406
assafkatz99 <assafkatz99@gmail.com>
2023-01-02 14:41:31 +0200

    day 13 additions

--- main.js
+++ main.js
@@ -41,3 +41,37 @@ function darnkModeFunction() {
   }
   food_list.innerHTML += "</ol>"
 }
+
+
+const imgArr =[
+  {
+    imgLink : "https://upload.wikimedia.org/wikipedia/commons/9/94/2013_Australian_Open_-_Guillaume_Rufin.jpg",
+    description : "Tennis is a racquet sport played by two or four players in which a ball is hit back and forth across a net.",
+    id : "imgNum1"
+  },
+  {
+    imgLink : "http://reneeroaming.com/wp-content/uploads/2020/08/Best-National-Park-Road-Trip-Itinerary-Grand-Teton-National-Park-Van-Life-819x1024.jpg",
+    description : "Roadtripping is the act of traveling long distances by car, often for leisure or vacation.",
+    id : "imgNum2"
+  },
+  {
+    imgLink : "https://www.goya.com/media/7864/hummus.jpg?quality=80",
+    description : "Hummus is a Middle Eastern dip made from chickpeas, tahini, lemon juice, and spices.",
+    id : "imgNum3"
+  },
+]
+
+function sendImgArrToHtmlUsingId (imgarr, elementsId){
+  let htmlCode = ""
+  for (let i = 0; i < imgarr.length; i++){
+    htmlCode += "<div>"
+    htmlCode += `<img id= ${imgarr[i].id} src=${imgarr[i].imgLink} /><p style = "width :130px" >${imgarr[i].description}</p>`
+    // console.log(document.getElementById(imgarr.id).clientWidth)
+    // htmlCode += `<img id= ${imgarr[i].id} src=${imgarr[i].imgLink} />`
+    // htmlCode += `<p style="width: ${document.getElementById(imgarr[i].id).clientWidth}px;">${imgarr[i].description}</p>`
+    htmlCode += "</div>"
+  }
+  document.getElementById(elementsId).innerHTML = htmlCode
+}
+
+sendImgArrToHtmlUsingId(imgArr,"imgSection")
